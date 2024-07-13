04148379c1b670e4ae15fb184d892cd4c4b1c0bf
eduardo.brito <eduardo.brito@plannersistemas.com.br>
2024-07-13 00:53:43 -0300

    refactor: refactor and add functions

--- js/app.js
+++ js/app.js
@@ -1,9 +1,19 @@
 let amigos = []
 function adicionar() {
-  let campoNomeAmigo = document.getElementById('nome-amigo')
-  let nomeAmigo = campoNomeAmigo.value
-  incluirAmigo(nomeAmigo)
-  campoNomeAmigo.value = ''
+  let amigo = document.getElementById('nome-amigo')
+  let lista = document.getElementById('lista-amigos')
+
+  amigos.push(amigo.value)
+
+  if(lista.textContent == '') {
+    lista.textContent = amigo.value
+  } else {
+    lista.textContent += `, ${amigo.value}`
+  }
+
+  amigo.value = ''
+  atualizarLista()
+  atualizarSorteio()
 }
 
 function sortear() {
@@ -19,21 +29,43 @@ function sortear() {
   }
 }
 
-function incluirAmigo(nomeAmigo) {
-  amigos.push(nomeAmigo)
-
-  let campoAmigos = document.getElementById('lista-amigos')
-  if(campoAmigos.textContent != '') {
-    campoAmigos.textContent += `, ${nomeAmigo}`
-  } else {
-    campoAmigos.textContent = nomeAmigo
-  }
+function excluirAmigo(index) {
+  amigos.splice(index, 1);
+  atualizarLista();
+  atualizarSorteio();
 }
 
 function embaralhar(lista) {
   for (let indice = lista.length; indice; indice--) {
       const indiceAleatorio = Math.floor(Math.random() * indice);
       [lista[indice - 1], lista[indiceAleatorio]] = [lista[indiceAleatorio], lista[indice - 1]];
+  }
+}
+
+function atualizarSorteio() {
+  let sorteio = document.getElementById('lista-sorteio');
+  sorteio.innerHTML = '';
+}
+
+
+function atualizarLista() {
+  let lista = document.getElementById('lista-amigos');
+  lista.innerHTML = '';
+
+
+  for (let i = 0; i < amigos.length; i++) {
+      // Cria um elemento de parágrafo para cada amigo
+      let paragrafo = document.createElement('p');
+      paragrafo.textContent = amigos[i];
+
+      // Adiciona um evento de clique para excluir o amigo
+      paragrafo.addEventListener('click', function() {
+          excluirAmigo(i);
+      });
+
+
+      // Adiciona o parágrafo à lista
+      lista.appendChild(paragrafo);
   }
 }
 
